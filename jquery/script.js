0f03e26040c6ef88389d819c173a196a08708492
Brandon kwamou <brandonkwamou@gmail.com>
2023-03-05 12:32:36 +0100

    remove one comment

--- jquery/script.js
+++ jquery/script.js
@@ -7,7 +7,6 @@ var player2Color = "rgb(237, 45, 73)";
 var game_on = true;
 var table = $("table tr");
 
-// http://stackoverflow.com/questions/6139407/getting-td-by-index-with-jquery
 function reportWin(rowNum, colNum) {
 	console.log("You won starting at this row,col");
 	console.log(rowNum);
