58cc481f0d7ecdec4517e418cb092ca9db992cb2
Brandon kwamou <brandonkwamou@gmail.com>
2023-03-07 19:06:05 +0100

    model template connection

--- my_site/my_site/urls.py
+++ my_site/my_site/urls.py
@@ -18,7 +18,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('my_app/', include('my_app.urls'))
+    path('my_app/', include('my_app.urls')),
+    path('office/', include('office.urls'))
 ]
 
 handler404 = 'my_site.views.my_custom_page_not_found_view'
